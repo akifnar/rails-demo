dc336b089dd6887ce85d9fea0f7c2b3a03e86376
akifnar <akifnar@gmail.com>
2026-04-24 16:32:37 +0300

    product test's validation errors have been resolved

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -12,7 +12,7 @@ class ProductTest < ActiveSupport::TestCase
 
   test "product attributes must not be empty" do
     product = Product.new
-    length product.invalid?
+    assert product.invalid?
     assert product.errors[:title].any?
     assert product.errors[:description].any?
     assert product.errors[:price].any?
