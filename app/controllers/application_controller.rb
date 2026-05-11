c4f884d4baffb6144ad97285194847966c66b11c
akifnar <akifnar@gmail.com>
2026-05-11 21:49:21 +0300

    Add order for checkout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :set_visit_counter
+  private
+  def set_visit_counter
+    if session[:counter].nil?
+        session[:counter] = 0
+        session[:last_visit] = Time.now
+
+    else
+        session[:counter] += 1
+    end
+      @session_counter =  session[:counter]
+  end
 end
