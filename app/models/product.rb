6e21d0cdf616d7c580b4a89f2d5beb3af77aa1c3
akifnar <akifnar@gmail.com>
2026-05-29 01:52:04 +0300

    Add rich text to products and display all orders in support requests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,12 @@
 class Product < ApplicationRecord
   has_many :line_items
   has_many :orders, through: :line_items
+  has_rich_text :rich_description
 
   before_destroy :ensure_not_referenced_by_any_line_item
 
 
-  validates :title, :description, :image_url, presence: true
+  validates :title, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01, message: "must be greater than or equal to 0.01" }
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png|webp)\z}i,
@@ -13,7 +14,6 @@ class Product < ApplicationRecord
   }
   validates :title, uniqueness: true
   validates :title, length: { minimum: 10, message: "minimum 10 characters" }
-
 
   def price_in_locale
     rate = I18n.t("currency_settings.rate", default: 1.0).to_f
--- app/models/support_request.rb
+++ app/models/support_request.rb
@@ -1,4 +1,8 @@
 class SupportRequest < ApplicationRecord
-  belongs_to :order, optional: true
+  has_many :orders
   has_rich_text :response
+
+  def orders
+    Order.where(email: self.email).order("created_at DESC").limit(2)
+  end
 end
