d419132e630ff4c8d547c915b767a614c9a21fdd
akifnar <akifnar@gmail.com>
2026-05-28 11:27:24 +0300

    Add support mailbox and rich-text response features

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  get "admin" => "admin#index"
 
   controller :sessions do
     get "login" => :new
@@ -8,11 +7,8 @@ Rails.application.routes.draw do
     delete "logout" => :destroy
   end
 
-
   resources :products do
-    member do
-      get "who_bought"
-    end
+    get "who_bought", on: :member
   end
 
   resources :users do
@@ -25,28 +21,21 @@ Rails.application.routes.draw do
   resources :users
   resources :pay_types
 
+
+  get "admin" => "admin#index"
+
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+
+  resources :support_requests, only: %i[ index update]
+
+
   scope "(:locale)" do
     resources :orders
     resources :line_items
     resources :carts
     root "store#index", as: "store_index", via: :all
   end
-
-
-  get "store/index"
-
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-   # Defines the root path route ("/")
-   # root "posts#index"
-
 end
